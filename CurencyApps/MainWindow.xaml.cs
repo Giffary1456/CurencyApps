57697bd19ae0a2379462f0ea006e898738a1af0c
giffary zidane <giffary1456@outlook.com>
2020-10-13 17:26:51 +0700

    Add project files.

---
+++ CurencyApps/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurencyApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CurencyController curency;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            curency = new CurencyController();
+        }
+
+        private void Button_Hitung_Click(object sender, RoutedEventArgs e)
+        {
+            var nominalInput = InputTextBox.Text;
+            var result = "Invalid";
+            if (curency.isAllowed(nominalInput));
+            {
+                result = curency.usdToIdr(nominalInput);
+            }
+            resultLabel.Content = result;
+        }
+    }
+}
